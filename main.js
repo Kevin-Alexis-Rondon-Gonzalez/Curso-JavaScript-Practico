fefc777a05472ab8abbc53413544450bbece9c56
Kevin Alexis Rondon Gonzalez <kevinalexisrondongonzalez@gmail.com>
2022-09-15 22:29:12 -0500

    Integrado el menu mobile y ajuste de @media para ambos menus

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+//Menus desktop y mobil
 const navMenuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBurger = document.querySelector(".menu-burger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 navMenuEmail.addEventListener("click", toggleEmailDesktopMenu);
+menuBurger.addEventListener("click", toggleMobileMenu);
+
 
 function toggleEmailDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
